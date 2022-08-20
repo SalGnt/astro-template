f9d8651e8c550077af0119568aa0d93702a65f63
Salvatore Gentile <884093+SalGnt@users.noreply.github.com>
2022-08-20 14:58:52 +0200

    Add @astrojs/sitemap integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,11 @@
 import { defineConfig } from "astro/config";
-import tailwind from "@astrojs/tailwind";
+
 import image from "@astrojs/image";
+import sitemap from "@astrojs/sitemap";
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [image(), tailwind()],
+  integrations: [image(), sitemap(), tailwind()],
+  site: "https://example.com",
 });
